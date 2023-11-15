9cb924309f2b7cb2f2b5082c61bf17bb625e0512
Nate <100454430+Noxolo1@users.noreply.github.com>
2023-11-15 09:58:33 -0800

    hw3 update 1

--- hw3/src/kMeansCluster.java
+++ hw3/src/kMeansCluster.java
@@ -1,7 +1,26 @@
 public class kMeansCluster {
 
+    public static  importData(){}
 
+
+    public static void kMeans(){
+        /* Inputs -> D – dataset with n objects
+        • Output-> set of k clusters
+        • Method
+        (1) arbitrarily select k objects from D as the initial
+        cluster mean (centroids)
+        (2) reassign each object to the cluster to which it is most
+        similar, based upon the distance between the object
+        and the cluster centroid
+        (3) update cluster centroid using objects in each cluster
+        (4) Repeat (2) and (3) until the criterion function
+        converges (no redistribution of objects in any cluster)*/
+
+
+
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
